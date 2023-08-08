8050c3b9141a430153ff1675e6450a1a171546b9
carternadain <cnadain@gmail.com>
2023-08-07 21:27:50 -0700

    complete gmap , loadscript fix all errors

--- road-tripper/src/components/Map/Map.js
+++ road-tripper/src/components/Map/Map.js
@@ -1,43 +1,45 @@
-// Map.js
 import React, { useEffect, useRef } from 'react';
+import { GoogleMap, LoadScript, } from '@react-google-maps/api';
 
 const Map = ({ destination }) => {
   const mapRef = useRef(null);
   const map = useRef(null);
 
   useEffect(() => {
-    // Load the Google Maps API script dynamically
-    const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/AIzaSyDHvUfktBNhORjt0tl6SXmDPe47CePDp9A`;
-    script.onload = initMap;
-    document.head.appendChild(script);
-
-    // Clean up
-    return () => {
-      document.head.removeChild(script);
-    };
-  }, []);
-
-  const initMap = () => {
-    // Check if the map container element exists
     if (mapRef.current) {
       map.current = new window.google.maps.Map(mapRef.current, {
-        center: { lat: 40.7128, lng: -74.0060 }, // Set your initial map center
-        zoom: 12, // Set the initial zoom level
+        center: { lat: 40.7128, lng: -74.0060 }, // Default center
+        zoom: 12,
       });
     }
-  };
+  }, []);
 
-  // Update the map when the destination prop changes
   useEffect(() => {
-    if (map.current) {
-      // Add your logic here to update the map with the new destination
-      // For example, you can use the Geocoding API to convert the destination address to latlng and update the map center
-      // Note: Make sure to handle errors and edge cases in your implementation
+    if (map.current && destination) {
+      const geocoder = new window.google.maps.Geocoder();
+      geocoder.geocode({ address: destination }, (results, status) => {
+        if (status === 'OK' && results[0]) {
+          map.current.setCenter(results[0].geometry.location);
+        } else {
+          console.error('Geocode was not successful for the following reason:', status);
+        }
+      });
     }
   }, [destination]);
 
-  return <div ref={mapRef} style={{ width: '100%', height: '400px' }} />;
+  return (
+    <div style={{ width: '100%', height: '400px' }}>
+      <LoadScript googleMapsApiKey="AIzaSyDHvUfktBNhORjt0tl6SXmDPe47CePDp9A">
+        <GoogleMap
+          mapContainerStyle={{ width: '100%', height: '100%' }}
+          center={{ lat: 40.7128, lng: -74.0060 }}
+          zoom={12}
+        >
+         
+        </GoogleMap>
+      </LoadScript>
+    </div>
+  );
 };
 
 export default Map;
