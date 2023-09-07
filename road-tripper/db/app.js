4fde598f98055f0dc3b0db1a555e49aca61d8be5
carternadain <cnadain@gmail.com>
2023-09-06 18:14:41 -0700

    add some new files for log in / static code

--- road-tripper/db/app.js
+++ road-tripper/db/app.js
@@ -1,16 +1,47 @@
 // app.js
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
-const port = 5000; // Choose a port for your API
+// const port = 5000; // Choose a port for your API
+const session = require('express-session');
+const passport = require('passport');
+const User = require('./models/user');
 
-// Middleware
-app.use(cors());
-app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
-// Define your API routes here (e.g., /api/nationalparks, /api/activities)
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const newUser = new User({ username, password });
+    await newUser.save();
+    res.redirect('/login'); // Redirect to login page after successful registration
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error registering user.');
+  }
 });
+
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/dashboard',
+  failureRedirect: '/login',
+  failureFlash: true, // Enable flash messages for failed login
+}));
+
+// ... Other routes ...
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+// // Middleware
+// app.use(cors());
+// app.use(express.json());
+
+// // Define your API routes here (e.g., /api/nationalparks, /api/activities)
+
+// // Start the server
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
---
+++ road-tripper/db/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true },
+  password: String,
+});
+
+// Hash the user's password before saving it to the database
+userSchema.pre('save', async function(next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(user.password, salt);
+  user.password = hash;
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
