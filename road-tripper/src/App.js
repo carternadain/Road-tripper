6c1005500cdefaac34a62f1df9af6077b6bed776
carternadain <cnadain@gmail.com>
2023-11-07 21:12:58 -0700

    update comments

--- road-tripper/src/App.js
+++ road-tripper/src/App.js
@@ -8,18 +8,15 @@ import Footer from './components/Footer/Footer';
 const App = () => {
   const [attractions, setAttractions] = useState([]); // State to store attractions
 
-  // Function to search for attractions
+ 
   const searchAttractions = (query) => {
-    // Perform a search for attractions based on the 'query'
-    // Update the 'attractions' state with the search results
+    
 
-    // ...
-
-    // For simplicity, you can set the 'attractions' state with a mock array for now.
+  
     const mockAttractions = [
       { name: 'National Park 1', location: { lat: 41.7128, lng: -74.0060 } },
       { name: 'National Park 2', location: { lat: 42.7128, lng: -74.0060 } },
-    ];  // Add more attractions here
+    ];  
     setAttractions(mockAttractions);
   };
 
@@ -27,8 +24,8 @@ const App = () => {
     <div>
       <Navbar />
       <Hero />
-      <Trip onSearch={searchAttractions} /> {/* Pass the search function to the Trip component */}
-      <Map attractions={attractions} /> {/* Pass the attractions to the Map component */}
+      <Trip onSearch={searchAttractions} /> 
+      <Map attractions={attractions} /> 
       <Footer />
     </div>
   );
