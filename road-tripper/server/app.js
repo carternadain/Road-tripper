2e4be5bf6405f016354f1b7d531cfa08d47ebb26
carternadain <cnadain@gmail.com>
2023-11-07 21:39:34 -0700

    work on database re config middleware

--- road-tripper/server/app.js
+++ road-tripper/server/app.js
@@ -1,12 +1,44 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000; // Choose a port for your app
+const port = process.env.PORT || 3000;
+const session = require('express-session');
+const passport = require('passport');
+
+app.use(session({
+  secret: 'Afds!-fmvN-wmvL', 
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-// Start the server
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/dashboard',
+  failureRedirect: '/login',
+  failureFlash: true,
+}));
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+app.get('/dashboard', isAuthenticated, (req, res) => {
+  res.send('Dashboard Page');
+});
+
+function isAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
